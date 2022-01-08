6032489f71ebc625a873dde817e5e065db78c3ec
Sawom <asawom@gmail.com>
2022-01-08 12:15:34 +0600

    first commit

---
+++ js/books.js
@@ -0,0 +1,32 @@
+// load book information
+const loadBook = () => {
+    const getInput = document.getElementById('inputField');
+    const getInputText = getInput.value ;
+    getInput.value = '';
+    const url = `https://openlibrary.org/search.json?q=${getInputText}` ;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => showBook(data.docs))
+}
+// show results
+const showBook = (books) => {
+    // console.log(books);
+    const displayBook = document.getElementById('displayBook');
+    displayBook.textContent = '';
+    books.forEach(book => {
+        // console.log(book)
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card h-100">
+            <img src='https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg' " class="card-img-top" alt="...">
+            <div class="card-body">
+                <h4 class="card-title">Book name: ${book.title}</h4>
+                <h5 class="card-title">First Publish: ${book.first_publish_year}</h5>
+                <p class="card-text">Author : ${book.author_name}</p>
+            </div>
+        </div>
+        `;
+        displayBook.appendChild(div);
+    })
+}
